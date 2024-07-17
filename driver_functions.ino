de59074d52fb155ee788b13eae40a51c3781fc20
Logdrak4 <33069578+Logdrak4@users.noreply.github.com>
2024-07-17 11:43:43 -0400

    Added comments for clarity

--- driver_functions.ino
+++ driver_functions.ino
@@ -13,7 +13,7 @@
 #define ECHO_LEFT  14 // Ultrasonic Echo pin
 
 
-#define INTERRUPT_PIN 2  // use pin 2 on Arduino Uno & most boards CHANGE LATER
+#define INTERRUPT_PIN 2  // use pin 2 on Arduino Uno & most boards. CHANGE LATER
 
 
 NewPing sonar[3] = {
@@ -23,22 +23,23 @@ NewPing sonar[3] = {
 };
 
 /////////////////////ENCODER MOTOR VARS/////////////////////////////////////
-int encoderPin1 = 2; //Encoder Output 'A' must connected with intreput pin of arduino.
-int encoderPin2 = 3; //Encoder Otput 'B' must connected with intreput pin of arduino.
+
+#define encoderPin1 2; //Encoder Output 'A' must connected with intreput pin of arduino.
+#define encoderPin2 3; //Encoder Otput 'B' must connected with intreput pin of arduino.
+#define encoder2Pin1 7;
+#define encoder2Pin2 8;
+#define PPR 7
+
 volatile int lastEncoded = 0; // Here updated value of encoder store.
 volatile long encoderValue = 0; // Raw encoder value
 volatile int lastEncoded2 = 0; // Here updated value of encoder store.
 volatile long encoderValue2 = 0; // Raw encoder value
-const int PWMright1 = 5;
-const int PWMright2 = 6;
-const int PWMleft1 = 9;
-const int PWMleft2 = 10;
 
-// PCICR | = B00000101;
-// PCMSK | = B00000001;
-// PCMSK2 | = B00000100;
-#define encoder2Pin1 7
-#define encoder2Pin2 8
+#define PWMright1 5;
+#define PWMright2 6;
+#define PWMleft1 9;
+#define PWMleft2 10;
+
 
 //////////////// MPU control/status vars///////////////////////////////////
 MPU6050 mpu;
@@ -54,15 +55,8 @@ Quaternion q;           // [w, x, y, z]         quaternion container
 float euler[3];         // [psi, theta, phi]    Euler angle container
 ////////////////////////////COLOR SENSOR/////////////////////////////////
   
-// const int s2 = A6;  
-// const int s3 = A7;  
-// const int out = 13; 
-// int  Red=0, Blue=0, Green=0; 
-// bool middle = false;
-// int possible_turns[4]={1,1,1,1}; //Array to keep track of possible turns. {front, right, left, rear}. 1 = turn possble. 0 = no turn possible
-// tcs3200 tcs(1, 0, s2, s3, out); // (S0, S1, S2, S3, output pin) //  ---  see:  https://www.mouser.com/catalog/specsheets/TCS3200-E11.pdf
-//
 #define ADPS_I2C 0x39
+
 Adafruit_APDS9960 apds;
 uint16_t r, g, b, c;
 
@@ -95,50 +89,23 @@ void setup() {
   MPU_setup();
   APDS_setup();
 
-
-
 }
 
 void loop() {
- setSpeed(0,PWMright1);
- setSpeed(100,PWMright2);
- setSpeed(0,PWMleft1);
- setSpeed(100,PWMleft2);
-
-// // for (int i = 0; i <= 100; i++){
-// //  Serial.print("Forward  ");
-// //  Serial.println(encoderValue);
-// // }
-
-delay(5000);
-
-setSpeed(100,PWMright1);
-setSpeed(0,PWMright2);
-setSpeed(100,PWMleft1);
-setSpeed(0,PWMleft2);
-
-delay(5000);
-
-APDS_GetColors();
-// for (int i = 0; i <= 100; i++){
-//  Serial.print("Reverse  ");
-//  Serial.println(encoderValue);
-// }
+  
+motors_stop(true,100);
 delay(1000);
-if (mpu.dmpGetCurrentFIFOPacket(fifoBuffer)) {
-  // display Euler angles in degrees
-  mpu.dmpGetQuaternion(&q, fifoBuffer);
-  mpu.dmpGetEuler(euler, &q);
-  Serial.print("euler\t");
-  Serial.print(euler[0] * 180/M_PI);
-  Serial.print("\t");
-  Serial.print(euler[1] * 180/M_PI);
-  Serial.print("\t");
-  Serial.println(euler[2] * 180/M_PI);
-}
-// read_ultra(3,true);
-// GetColors();
+motors_straight(true,100);
+delay(1000);
+motors_stop(true,100);
+delay(1000);
+motors_straight(true,50);
+
+delay(5000);
+APDS_GetColors();
 CheckifSolved();
+delay(500);
+read_ultra(3,true);
 delay(200);
 
 } 
@@ -172,67 +139,83 @@ void updateEncoder2(){
   lastEncoded2 = encoded; //store this value for next time
 }
 
+//R_L: true right(1), false left(2)
+long getRotations(bool R_L){
+  if(R_L){
+    return encoderValue/PPR;  //divide by Pulses per rotation to get total number of rotations recorded
+  } else{
+    return encoderValue2/PPR; //divide by Pulses per rotation to get total number of rotations recorded
+  }
+}
+//to be used after a turn is made to ensure proper count
+void resetEncoders(){
+  encoderValue=0;
+  encoderValue2=0;
+}
+
 /////////////////////////////////////////////MOTORS////////////////////////////////////////////////////
-//speed in percentage
+//speed in percentage of duty cycle. i.e speed = 50 => 50% duty cycle
 void setSpeed(float speed, int pin){
   // analogWriteResolution(8);
   float percent = float(speed/100);
-  float dutycycle = float(255*percent);
-  analogWrite(pin, dutycycle); //or digitalwrite????
+  float dutycycle = float(255*percent); //255 is max of 8 bit resolution
+  analogWrite(pin, dutycycle); //send PWM to H bridge
 }
 
-// // R_L determines turn right or left. R_L = true : right turn; R_L = false : left turn;
-// void Turn(bool R_L, int degree, int turn_speed) 
-// {
-//     // while(degree!=90){
-//         if (R_L){
-//             setSpeed(turn_speed,PWMright1);
-//             setSpeed(0,PWMright2);
-//             setSpeed(turn_speed,PWMleft1);
-//             setSpeed(0,PWMleft2);
-//         }
-//         else{
-//             setSpeed(turn_speed,PWMright1);
-//             setSpeed(0,PWMright2);
-//             setSpeed(turn_speed,PWMleft1);
-//             setSpeed(0,PWMleft2);
-//         }
-//     // }
-// }
-// //R_L_BOTH: 0=right motor only; 1=left motor only; 2=both motors
-// void motors_stop(int R_L_BOTH) 
-// {
-//     if (R_L_BOTH == 0){
-//         setSpeed(0,PWMright1);
-//         setSpeed(0,PWMright2);
-//     }
-//     else if (R_L_BOTH == 1){
-//         setSpeed(0,PWMleft1);
-//         setSpeed(0,PWMleft2);
-//     }
-//     else if (R_L_BOTH == 2){
-//         setSpeed(0,PWMright1);
-//         setSpeed(0,PWMright2);
-//         setSpeed(0,PWMleft1);
-//         setSpeed(0,PWMleft2);
-//     }
-// }
-// //direction: true=forwad; false=backwards
-// void motors_straight(bool direction, int speed) 
-// {
-//     if (direction){
-//         setSpeed(0,PWMright1);
-//         setSpeed(speed,PWMright2);
-//         setSpeed(speed,PWMleft1);
-//         setSpeed(0,PWMleft2);
-//     }
-//     else{
-//         setSpeed(speed,PWMright1);
-//         setSpeed(0,PWMright2);
-//         setSpeed(0,PWMleft1);
-//         setSpeed(speed,PWMleft2);
-//     }
-// }
+// R_L determines turn right or left. R_L = true : right turn; R_L = false : left turn;
+void Turn(bool R_L, int degree, int turn_speed) 
+{
+    // while(MPU_getX()!=90){
+        if (R_L){
+            setSpeed(turn_speed,PWMright1);
+            setSpeed(0,PWMright2);
+            setSpeed(turn_speed,PWMleft1);
+            setSpeed(0,PWMleft2);
+        }
+        else{
+            setSpeed(turn_speed,PWMright1);
+            setSpeed(0,PWMright2);
+            setSpeed(turn_speed,PWMleft1);
+            setSpeed(0,PWMleft2);
+        }
+    // }
+    motors_stop();    //stop turning 
+    resetEncoders();  //reset encoders to count for next turn
+}
+//R_L_BOTH: 0=right motor only; 1=left motor only; 2=both motors
+void motors_stop(int R_L_BOTH) 
+{
+    if (R_L_BOTH == 0){ //stop Right motor
+        setSpeed(0,PWMright1);
+        setSpeed(0,PWMright2);
+    }
+    else if (R_L_BOTH == 1){ //stop left motor
+        setSpeed(0,PWMleft1);
+        setSpeed(0,PWMleft2);
+    }
+    else if (R_L_BOTH == 2){ //stop both motors
+        setSpeed(0,PWMright1);
+        setSpeed(0,PWMright2);
+        setSpeed(0,PWMleft1);
+        setSpeed(0,PWMleft2);
+    }
+}
+//direction: true=forward; false=backwards. NEED TO BE CALIBRATED BASED ON ORIENTATION OF MOTORS IN CHASSIS
+void motors_straight(bool direction, int speed) 
+{
+    if (direction){ //forward direction
+        setSpeed(0,PWMright1);
+        setSpeed(speed,PWMright2);
+        setSpeed(speed,PWMleft1);
+        setSpeed(0,PWMleft2);
+    }
+    else{ //backwards
+        setSpeed(speed,PWMright1);
+        setSpeed(0,PWMright2);
+        setSpeed(0,PWMleft1);
+        setSpeed(speed,PWMleft2);
+    }
+}
 
 
 ///////////////////////////////////////////////////////////Ultrasonic/////////////////////////////////////////////////
@@ -255,7 +238,7 @@ void updatePossibleTurns()
         possible_turns[2]=0;
     }
 }
-
+//prints each sensor's distance depending. which => 0 = none; 1 = front sensor; 2 =  right sensor; 3 = left sensor; recurse=> true = which downto 0
 void read_ultra(int which, bool recurse){
     double cm;
     
@@ -296,11 +279,10 @@ void APDS_setup(){
   //enable color sensign mode
   apds.enableColor(true);
 }
+//display rgb values
 void APDS_GetColors(){
 
   // Wire.beginTransmission(ADPS_I2C);  // read from device
-
-
    //wait for color data to be ready
   while(!apds.colorDataReady()){
     delay(5);
@@ -322,43 +304,10 @@ void APDS_GetColors(){
   Serial.println();
 
   // Wire.endTransmission();      // Stop transmitting
-  delay(500);
+  delay(100);
   
 }
-
-// void GetColors()  
-// {    
-//   #define num_of_colors 7   // Declares the number of colors the program can recognise (number of calibrated colors)
-
-//   // distinctRGB[] array declares calibration values for each declared color in distinctColors[] array
-//   int distinctRGB[num_of_colors][3] = {{250, 250, 250}, {8,8 , 8}, {142, 34, 41}, {166, 125, 71}, {35, 55, 38}, {150, 50, 43}, {12, 12, 12}};
-//   // distinctColors[] array declares values to be returned from closestColor() function if specified color is recognised
-//   String distinctColors[num_of_colors] = {"white", "black", "red", "yellow", "green", "orange", "blue"};
-
-//   int red, green, blue;
-//   Serial.println(tcs.closestColor(distinctRGB, distinctColors, num_of_colors) );
-
-//   // red = tcs.colorRead('r');   //reads color value for red
-//   // Serial.print("R= ");
-//   // Serial.print(red);
-//   // Serial.print("    ");
-  
-//   // green = tcs.colorRead('g');   //reads color value for green
-//   // Serial.print("G= ");
-//   // Serial.print(green);
-//   // Serial.print("    ");
-
-//   // blue = tcs.colorRead('b');    //reads color value for blue
-//   // Serial.print("B= ");
-//   // Serial.print(blue);
-//   // Serial.print("    ");
-
-//   Serial.println();
-
-//   delay(200);
-// }
-
-
+//determine color from rgb values and see if blue center is reached
 void CheckifSolved(){
     if (r > g && r > b ) {       
       Serial.println("- RED detected!");      
@@ -375,13 +324,46 @@ void CheckifSolved(){
     if(g > r && g > b){
         Serial.println(" - Green detected!");
     }
-    if( g < 12 && r < 12 && b < 12){
+    if( g < 12 && r < 12 && b < 12){  //adjust based on lighting under the robot
       Serial.println(" - BLACK detected!");
 
     }
 }
 
 ////////////////////////////////////MPU FUNCTIONS////////////////////////////////////
+
+void MPU_getdata(){
+  if (mpu.dmpGetCurrentFIFOPacket(fifoBuffer)) {
+  // display Euler angles in degrees
+  mpu.dmpGetQuaternion(&q, fifoBuffer);
+  mpu.dmpGetEuler(euler, &q);
+  Serial.print("euler\t");
+  Serial.print(euler[0] * 180/M_PI);
+  Serial.print("\t");
+  Serial.print(euler[1] * 180/M_PI);
+  Serial.print("\t");
+  Serial.println(euler[2] * 180/M_PI);
+}
+}
+//return X degrees
+float MPU_getX(){
+  MPU_getdata();
+  float X_degrees=(euler[0] * 180/M_PI);
+  return X_degrees;
+}
+//return y degrees
+float MPU_getY(){
+  MPU_getdata();
+  float Y_degrees=(euler[1] * 180/M_PI);
+  return Y_degrees;
+}
+//return z degrees
+float MPU_getZ(){
+  MPU_getdata();
+  float Z_degrees=(euler[2] * 180/M_PI);
+  return Z_degrees;
+}
+
 void MPU_setup() {
     // join I2C bus (I2Cdev library doesn't do this automatically)
     #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
