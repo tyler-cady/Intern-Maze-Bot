1e2092e25559cda355b5e0eb84ae913ece6a5507
Logdrak4 <33069578+Logdrak4@users.noreply.github.com>
2024-07-18 16:08:15 -0400

    efficency

--- driver_functions.ino
+++ driver_functions.ino
@@ -14,15 +14,16 @@
 
 #define INTERRUPT_PIN 2  // use pin 2 on Arduino Uno & most boards. CHANGE LATER
 
-
 NewPing sonar[3] = {
+  NewPing(TRIGGER_PIN, ECHO_LEFT,30),
   NewPing(TRIGGER_PIN, ECHO_FRONT,30),
-  NewPing(TRIGGER_PIN, ECHO_RIGHT,30),
-  NewPing(TRIGGER_PIN, ECHO_LEFT,30)
+  NewPing(TRIGGER_PIN, ECHO_RIGHT,30)
 };
 
-/////////////////////ENCODER MOTOR VARS/////////////////////////////////////
 
+float cm[3]; //left,front,right
+bool walls[3]; //left,front, right
+/////////////////////ENCODER MOTOR VARS/////////////////////////////////////
 #define encoderPin1 11 //Encoder Output 'A' must connected with intreput pin of arduino.
 #define encoderPin2 12 //Encoder Otput 'B' must connected with intreput pin of arduino.
 #define encoder2Pin1 7
@@ -53,11 +54,11 @@ uint8_t fifoBuffer[64]; // FIFO storage buffer
 Quaternion q;           // [w, x, y, z]         quaternion container
 float euler[3];         // [psi, theta, phi]    Euler angle container
 ////////////////////////////COLOR SENSOR/////////////////////////////////
-  
 #define ADPS_I2C 0x39
 
 Adafruit_APDS9960 apds;
 uint16_t r, g, b, c;
+
 
 
 void setup() {
@@ -121,7 +122,6 @@ void updateEncoder(){
   if(sum == 0b1110 || sum == 0b0111 || sum == 0b0001 || sum == 0b1000) encoderValue --;
 
   lastEncoded = encoded; //store this value for next time
-
 }
 void updateEncoder2(){
   int MSB = digitalRead(encoder2Pin1); //MSB = most significant bit
@@ -135,7 +135,6 @@ void updateEncoder2(){
 
   lastEncoded2 = encoded; //store this value for next time
 }
-
 //R_L: true right(1), false left(2)
 int getRotations(bool R_L){
   if(R_L){
@@ -150,6 +149,8 @@ void resetEncoders(){
   encoderValue=0;
   encoderValue2=0;
 }
+
+//GET RID OF AFTER TESTING FOR OUPTUT PURPOSES ONLY
 void readEncoders(){
  Serial.print("Encoder1: ");
  Serial.print(encoderValue, DEC);
@@ -170,7 +171,7 @@ void setSpeed(float speed, int pin){
 // R_L determines turn right or left. R_L = true : right turn; R_L = false : left turn;
 void Turn(bool R_L, int degree, int turn_speed) 
 {
-    // while(MPU_getX()!=90){
+    // while(MPU_getX()!=degree){
         if (R_L){
             setSpeed(turn_speed,PWMright1);
             setSpeed(0,PWMright2);
@@ -221,56 +222,29 @@ void motors_straight(bool direction, int speed)
         setSpeed(speed,PWMleft2);
     }
 }
-
-
 ///////////////////////////////////////////////////////////Ultrasonic/////////////////////////////////////////////////
-
-// void updatePossibleTurns()
-// {
-//     if (sonar[0].ping_cm() < 2){
-//         Serial.println("Wall front");
-//         possible_turns[0]=0;
-//     }
-//     else{
-//         Serial.println("No wall front");
-//     }
-//     if (sonar[1].ping_cm()  < 2){
-//         Serial.println("Wall Right");
-//         possible_turns[1]=0;
-//     }
-//     if (sonar[2].ping_cm()  < 2){
-//         Serial.println("Wall left");
-//         possible_turns[2]=0;
-//     }
-// }
 //prints each sensor's distance depending. which => 0 = none; 1 = front sensor; 2 =  right sensor; 3 = left sensor; recurse=> true = which downto 0
 void read_ultra(int which, bool recurse){
-    double cm;
-    
     if(which == 0){
         return;
     }
     if(which == 1){
-        cm = sonar[0].ping_cm(30) ;
-        Serial.print("Distance front: "); 
-        Serial.print(cm, DEC);  
+		cm[0] = sonar[0].ping_cm(30) ;
+        Serial.print("Distance left: ");
+        Serial.print(cm, DEC);
         Serial.println(" ");
-        if( cm < 5.0 ){
-          motors_stop(2);
-        } else {
-          motors_straight(true,100);
-        }
+
     }
     if(which == 2){
-        double right_cm = sonar[1].ping_cm(30) ;
-        Serial.print("Distance right: ");
-        Serial.print(right_cm, DEC);
+		cm[1] = sonar[1].ping_cm(30) ;
+        Serial.print("Distance front: "); 
+        Serial.print(cm[1], DEC);  
         Serial.println(" ");
     }
     if(which == 3){
-        cm = sonar[2].ping_cm(30) ;
-        Serial.print("Distance left: ");
-        Serial.print(cm, DEC);
+        cm[2] = sonar[2].ping_cm(30) ;
+        Serial.print("Distance right: ");
+        Serial.print(cm[2], DEC);
         Serial.println(" ");
     }
     if(recurse){
@@ -279,6 +253,13 @@ void read_ultra(int which, bool recurse){
         read_ultra(which,recurse);
     }
 }
+void checkWalls(){ //make the front one different. could be error.
+	 for(i=0;i<cm.length;i++){
+		 if(cm[0]<10){
+			 walls[i]=true;
+		 }
+	 }
+}
 //////////////////////////////COLOR SENSOR FUNCTIONS/////////////////////////////////////////
 
 void APDS_setup(){
@@ -286,19 +267,16 @@ void APDS_setup(){
     Serial.println("failed to initialize device! Please check your wiring.");
   }
   else Serial.println("APDS initialized!");
-
   //enable color sensign mode
   apds.enableColor(true);
 }
 //display rgb values
 void APDS_GetColors(){
-
   // Wire.beginTransmission(ADPS_I2C);  // read from device
    //wait for color data to be ready
   while(!apds.colorDataReady()){
     delay(5);
   }
-
   //get the data and print the different channels
   apds.getColorData(&r, &g, &b, &c);
   Serial.print("red: ");
@@ -309,41 +287,20 @@ void APDS_GetColors(){
   
   Serial.print(" blue: ");
   Serial.print(b);
-  
-  Serial.print(" clear: ");
-  Serial.println(c);
-  Serial.println();
-
   // Wire.endTransmission();      // Stop transmitting
   delay(100);
   
 }
 //determine color from rgb values and see if blue center is reached
 void CheckifSolved(){
-    if (r > g && r > b ) {       
-      Serial.println("- RED detected!");  
-      motors_straight(true,100);    
-      // middle=false;
-    }
     if(b > r && b > g){
-        Serial.println(" - BLUE detected!");
-        motors_stop(2);
-        // middle=true;
-    }
-    // else{
-    //     motors_straight(true, 100);
-    // }
-    if(g > r && g > b){
-        Serial.println(" - Green detected!");
-        motors_straight(true,100);
+       Serial.println(" - BLUE detected!");
+       middle=true;
     }
     if( g < 12 && r < 12 && b < 12){  //adjust based on lighting under the robot
       Serial.println(" - BLACK detected!");
-      motors_straight(true,100);
-
     }
 }
-
 ////////////////////////////////////MPU FUNCTIONS////////////////////////////////////
 
 void MPU_getdata(){
@@ -386,33 +343,16 @@ void MPU_setup() {
     #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
         Fastwire::setup(400, true);
     #endif
-
-    // initialize serial communication
-    // (115200 chosen because it is required for Teapot Demo output, but it's
-    // really up to you depending on your project)
-    // Serial.begin(9600);
     while (!Serial); // wait for Leonardo enumeration, others continue immediately
-
-    // NOTE: 8MHz or slower host processors, like the Teensy @ 3.3V or Arduino
-    // Pro Mini running at 3.3V, cannot handle this baud rate reliably due to
-    // the baud timing being too misaligned with processor ticks. You must use
-    // 38400 or slower in these cases, or use some kind of external separate
-    // crystal solution for the UART timer.
-
     // initialize device
     Serial.println(F("Initializing I2C devices..."));
     mpu.initialize();
     pinMode(INTERRUPT_PIN, INPUT);
-
     // verify connection
     Serial.println(F("Testing device connections..."));
     Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
-
-    // wait for ready
-    // Serial.println(F("\nSend any character to begin DMP programming and demo: "));
+	
     while (Serial.available() && Serial.read()); // empty buffer
-    // while (!Serial.available());                 // wait for data
-    // while (Serial.available() && Serial.read()); // empty buffer again
 
     // load and configure the DMP
     Serial.println(F("Initializing DMP..."));
@@ -458,10 +398,4 @@ void MPU_setup() {
         Serial.print(devStatus);
         Serial.println(F(")"));
     }
-
-}
-
-volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
-void dmpDataReady() {
-    mpuInterrupt = true;
 }
