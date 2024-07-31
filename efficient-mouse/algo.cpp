483648cc34737fc083104626daf0efd5674bbfb9
Tyler Cady <99786931+tyler-cady@users.noreply.github.com>
2024-07-31 11:08:19 -0400

    Update algo.cpp

--- efficient-mouse/algo.cpp
+++ efficient-mouse/algo.cpp
@@ -10,10 +10,11 @@ string optimizePath(string seeked) {
     // SBL = R
     // SBS = B
     // LBL = S
-    string opt; 
+    string opt;
     do {
-        for (int i = 0; i < seeked.length(); (i += 3)){
-            string temp = to_string(seeked[i] + seeked[i+1] + seeked[i+2]);
+        opt.clear();
+        for (int i = 0; i < seeked.length(); i += 3) {
+            string temp = seeked.substr(i, 3);
             if      (temp == "LBR") opt += "B";
             else if (temp == "LBS") opt += "R";
             else if (temp == "RBL") opt += "B";
@@ -22,9 +23,9 @@ string optimizePath(string seeked) {
             else if (temp == "LBL") opt += "S";
             else opt += temp;
         }
-        seeked = opt; 
-    } while ((!opt.find("B")) && (opt.length() != 0));
-    return opt; 
+        seeked = opt;
+    } while (opt.find("B") != std::string::npos);
+    return opt;
 }
 
 int main(){
