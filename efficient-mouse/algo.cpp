79855ade9fc63aa425a3793a0fc2dd11eebc8efe
Tyler Cady <99786931+tyler-cady@users.noreply.github.com>
2024-07-31 11:22:22 -0400

    Update algo.cpp

--- efficient-mouse/algo.cpp
+++ efficient-mouse/algo.cpp
@@ -4,32 +4,27 @@
 using namespace std;
 
 string optimizePath(string seeked) {
-    // LBR = B
-    // LBS = R
-    // RBL = B
-    // SBL = R
-    // SBS = B
-    // LBL = S
     string opt;
     do {
         opt.clear();
         for (int i = 0; i < seeked.length(); i += 3) {
             string temp = seeked.substr(i, 3);
             if      (temp == "LBR") opt += "B";
-            else if (temp == "LBS") opt += "R";
+            else if (temp == "LBF") opt += "R";
             else if (temp == "RBL") opt += "B";
-            else if (temp == "SBL") opt += "R";
-            else if (temp == "SBS") opt += "B";
-            else if (temp == "LBL") opt += "S";
+            else if (temp == "FBL") opt += "R";
+            else if (temp == "FBF") opt += "B";
+            else if (temp == "LBL") opt += "F";
             else opt += temp;
         }
         seeked = opt;
+        cout << seeked <<endl;
     } while (opt.find("B") != std::string::npos);
     return opt;
 }
 
 int main(){
-    string a = "FFFFRLRFRFFLRFFFRFFFFRFRRLRRLFRLRFFFFFFLRLRRRLFFFFLFFLLFFRRFFRFRFFFFRRLRFFFLLFFLFFFFRFFRFFRFRRRRLRLFLLRLFLLRRFFFRLFRRLRRLLRRLLFFLRLFFLFL";
+    string a = "FFFFRLRFRFFLBRFFFRFFFFRFRRBLRRLFRLRFFFBFFFLRLRBRRLFFFFLFFLLFBFRRFFRFRFFFFRRLBRFFBFLLFFLFFBFFRFFRFFRFRRRRLRLFLLBRLFLLBRRFFFRLFRRLRRLLRRLLFFLRLFFLFL";
     string b = optimizePath(a);
     cout << a << endl << b << endl;
 }
