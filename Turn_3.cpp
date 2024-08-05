3f2020d9a677d5dbecfa8d9059094e9b7b901d3a
Logdrak4 <33069578+Logdrak4@users.noreply.github.com>
2024-08-04 23:29:18 -0400

    Update Turn_3.cpp

--- Turn_3.cpp
+++ Turn_3.cpp
@@ -1,6 +1,6 @@
 void turnLeft90() {
 reset_encoders();
   while (abs(leftEncoderCount) < TURN_90_DEGREES_COUNTS && abs(rightEncoderCount) < TURN_90_DEGREES_COUNTS) {
-    Turn(
+    Turn(false,83,96);
   }
   stopMotors();
