c27d94b208a88b037f4e18bf15184e045119407f
Logdrak4 <33069578+Logdrak4@users.noreply.github.com>
2024-08-05 15:25:07 -0400

    Update pid.h

--- efficient-mouse/pid.h
+++ efficient-mouse/pid.h
@@ -1,13 +1,8 @@
-#ifndef PID_HPP
-#define PID_HPP
+  #ifndef PID_HPP
+  #define PID_HPP
 
-#include <fstream>
-#include <string>
-#include <chrono>
-#include <iomanip>
-
-class pid {
-private:
+  class pid {
+  private:
     double _max;
     double _min;
     double _Kp;
@@ -16,71 +11,40 @@ private:
     double _pre_error;
     double _integral;
 
-    std::ofstream _log_file;
-
-    // Function to get current time as a string
-    std::string current_time() {
-        auto now = std::chrono::system_clock::now();
-        auto time_t_now = std::chrono::system_clock::to_time_t(now);
-        auto local_time = std::localtime(&time_t_now);
-        
-        std::stringstream ss;
-        ss << std::put_time(local_time, "%Y-%m-%d %H:%M:%S");
-        return ss.str();
-    }
-
-    // Function to log data to a CSV file
-    void log(const std::string& time, double input, double output) {
-        cout << time << "," << input << "," << output << "\n";
-    }
-
-public:
-    pid(double Kp, double Ki, double Kd, double max = 255, double min = 0) 
-        : _max(max), _min(min), _Kp(Kp), _Kd(Kd), _Ki(Ki), _pre_error(0), _integral(0) {
-        // Open the log file in append mode
-        _log_file.open("pid_log.csv", std::ios::out | std::ios::app);
-    }
-
-    ~pid() {
-        if (_log_file.is_open()) {
-            _log_file.close();
-        }
-    }
+  public:
+    pid(double Kp, double Ki, double Kd, double max, double min = 0)
+        : _max(max), _min(min), _Kp(Kp), _Kd(Kd), _Ki(Ki), _pre_error(), _integral(0) {}
 
     double tick(double current, double desired, double dt) {
-        // Calculate error
-        double error = desired - current;
+      // Calculate error
+      double error = desired - current;
 
-        // Proportional term
-        double Pout = _Kp * error;
+      // Proportional term
+      double Pout = _Kp * error;
 
-        // Integral term
-        _integral += error * dt;
-        double Iout = _Ki * _integral;
+      // Integral term
+      _integral += error * dt;
+      double Iout = _Ki * _integral;
+      // Restrict integral to prevent growth after it saturates
+      if (_integral > _max) _integral = _max;
+      else if (_integral < _min) _integral = _min;
 
-        // Restrict integral to prevent growth after it saturates
-        if (_integral > _max) _integral = _max;
-        else if (_integral < _min) _integral = _min;
+      // Derivative term
+      double derivative = (error - _pre_error) / dt;
+      double Dout = _Kd * derivative;
 
-        // Derivative term
-        double derivative = (error - _pre_error) / dt;
-        double Dout = _Kd * derivative;
+      // Calculate total output
+      double output = Pout + Iout + Dout;
 
-        // Calculate total output
-        double output = Pout + Iout + Dout;
+      // Restrict to max/min
+      if (output > _max) output = _max;
+      else if (output < _min) output = _min;
 
-        // Restrict to max/min
-        if (output > _max) output = _max;
-        else if (output < _min) output = _min;
+      // Save error to previous error
+      _pre_error = error;
 
-        // Save error to previous error
-        _pre_error = error;
-
-        // Log the current time, input (current), and output
-        log(current_time(), current, output);
-
-        return output;
+      return output;
     }
-};
+  };
 
-#endif // end of PID_HPP
+  #endif // end of PID_HPP
